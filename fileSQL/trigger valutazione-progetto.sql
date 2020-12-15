e18bae5b76a00978d5835dd9bd85e0080611b761
mgcarofano <m.carofano@studenti.unina.it>
2020-12-15 22:57:03 +0100

    Update trigger valutazione-progetto.sql
    Risolti bug nel trigger TriggerInserimentoRecensione

--- fileSQL/trigger valutazione-progetto.sql
+++ fileSQL/trigger valutazione-progetto.sql
@@ -1,13 +1,9 @@
+--le recensioni sono valide se e solo se recensore e recensito hanno partecipato allo stesso progetto
 --le recensioni possono essere effettuate solo da impiegati che hanno avuto ruolo di project manager
---le recensioni sono valide sse recensore e recensito hanno partecipato allo stesso progetto
-
-CREATE VIEW ProgettoRecensore (idProgetto, Recensore, RuoloRecensore) AS
-(SELECT R.idProgetto, V.Recensore, R.Ruolo
- FROM Valutazione V JOIN Impiegato I ON V.Recensore = I.CF JOIN Registrazione R ON I.CF = R.CF)
-
-CREATE VIEW ProgettoRecensito (idProgetto, Recensito) AS
-(SELECT R.idProgetto, V.Recensito
- FROM Valutazione V JOIN Impiegato I ON V.Recensito = I.CF JOIN Registrazione R ON I.CF = R.CF)
+ 
+CREATE VIEW ProgettoRecensoreRecensito (idProgetto, Impiegato, Ruolo) AS
+(SELECT R.idProgetto, I.CF, R.Ruolo
+ FROM Impiegato I JOIN Registrazione R ON I.CF = R.CF)
  
 CREATE FUNCTION CKRecensione()
 RETURNS trigger
@@ -15,14 +11,18 @@ LANGUAGE 'plpgsql'
 AS $$
 BEGIN			
 	IF EXISTS (SELECT *
-			   FROM ProgettoRecensore NATURAL JOIN ProgettoRecensito
-			   WHERE Recensore = NEW.Recensore
-			   AND Recensito = NEW.Recensito) THEN --recensore e recensito hanno lavorato allo stesso progetto
+			   FROM ProgettoRecensoreRecensito PRR1, ProgettoRecensoreRecensito PRR2
+			   WHERE PRR1.Impiegato <> PRR2.Impiegato
+			   AND PRR1.idProgetto = PRR2.idProgetto
+			   AND PRR1.Impiegato = NEW.Recensore
+			   AND PRR2.Impiegato = NEW.Recensito) THEN --recensore e recensito hanno lavorato allo stesso progetto
 		IF NOT EXISTS (SELECT *
-					   FROM ProgettoRecensore NATURAL JOIN ProgettoRecensito
-					   WHERE Recensore = NEW.Recensore
-					   AND Recensito = NEW.Recensito
-					   AND RuoloRecensore = 'Project Manager') THEN
+			   		   FROM ProgettoRecensoreRecensito PRR1, ProgettoRecensoreRecensito PRR2
+			   		   WHERE PRR1.Impiegato <> PRR2.Impiegato
+			   		   AND PRR1.idProgetto = PRR2.idProgetto
+			   		   AND PRR1.Impiegato = NEW.Recensore
+			   		   AND PRR2.Impiegato = NEW.Recensito
+					   AND PRR1.Ruolo = 'Project Manager') THEN
 			RAISE NOTICE 'questo recensore non può scrivere recensioni perché non è un project manager del progetto';
 		ELSE
 			RETURN NEW;
@@ -31,7 +31,6 @@ BEGIN
 		RAISE NOTICE 'recensore e recensito non hanno lavorato allo stesso progetto';
 	END IF;
 END
-$$;
 
 CREATE TRIGGER TriggerInserimentoRecensione
 BEFORE INSERT ON Valutazione
