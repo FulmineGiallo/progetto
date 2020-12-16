09ef5db10686bb2c18d2769c9cbc9c629319beb5
Luca-Bianco <luca.bianco4@studenti.unina.it>
2020-12-16 10:17:02 +0100

    Update chiavi esterne
    Ho cancellato e ricreato le chiavi esterne di compambiti e registrazione perchè non avevano ON DELETE/UPDATE CASCADE

--- fileSQL/Tabella Registrazione.sql
+++ fileSQL/Tabella Registrazione.sql
@@ -3,7 +3,7 @@ Ruolo	VARCHAR(255),
 CF		VARCHAR(16),
 idAmbito 	INTEGER,
 
-CONSTRAINT Fk1FOREIGN KEY (CF) REFERENCES Impiegato (CF),
-CONSTRAINT Fk2 FOREIGN KEY (idAmbito) REFERENCES Ambiti (idAmbito),
+CONSTRAINT Fk1FOREIGN KEY (CF) REFERENCES Impiegato (CF) ON UPDATE CASCADE, ON DELETE CASCADE,
+CONSTRAINT Fk2 FOREIGN KEY (idAmbito) REFERENCES Ambiti (idAmbito) ON UPDATE CASCADE, ON DELETE CASCADE,
 CONSTRAINT CKRuolo CHECK (Ruolo IN ('Project Manager', 'Partecipante', 'Programmatore', 'Grafico', 'Analista', 'Tecnico', 'Consulente'))
 )	
