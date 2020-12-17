e1ea4f4302e7aa4df8f7b4def0993c176ab1c984
Luca-Bianco <luca.bianco4@studenti.unina.it>
2020-12-17 22:26:26 +0100

    Update trigger TriggerInserimentoProjectManager.sql
    Update trigger inserimento impiegato in progetto

--- fileSQL/trigger TriggerInserimentoProjectManager.sql
+++ fileSQL/trigger TriggerInserimentoProjectManager.sql
@@ -1,21 +1,35 @@
-CREATE FUNCTION public.ckregistrazione()
-    RETURNS trigger
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE NOT LEAKPROOF
-AS $BODY$
-BEGIN			
-		IF NOT EXISTS (SELECT *
-			   		   FROM Registrazione R
-			   		   WHERE R.idProgetto = NEW.idProgetto
-			   		   AND R.ruolo = 'Project Manager') THEN
-			RAISE NOTICE 'questo utente non puo essere registrato a questo progetto perche 	questo progetto non ha ancora un project manager';
-			RETURN NULL;
-		ELSE
-			RETURN NEW;
-		END IF;
-END
-$BODY$;
+CREATE FUNCTION CKRegistrazione()
+RETURNS trigger
 
-ALTER FUNCTION public.ckregistrazione()
-    OWNER TO postgres;
+AS '
+BEGIN			   
+		
+		IF NEW.ruolo='Project Manager' THEN
+		
+			IF EXISTS (SELECT *
+				   		   FROM Registrazione R
+				   		   WHERE R.idProgetto = NEW.idProgetto
+				   		   AND R.ruolo = 'Project Manager') THEN
+				RAISE NOTICE 'questo utente non puo essere registrato a questo progetto perche 	questo progetto ha già un project manager';
+				RETURN NULL;
+			ELSE
+				RETURN NEW;
+			END IF;
+		ELSE 
+			IF NOT EXISTS (SELECT *
+				   		   FROM Registrazione R
+				   		   WHERE R.idProgetto = NEW.idProgetto
+				   		   AND R.ruolo = 'Project Manager') THEN
+				RAISE NOTICE 'questo utente non puo essere registrato a questo progetto perche 	questo progetto non ha ancora un project manager';
+				RETURN NULL;
+			ELSE
+				RETURN NEW;
+		END IF
+	END IF	
+	END
+'LANGUAGE 'plpgsql';
+
+CREATE TRIGGER TriggerInserimentoProjectManager
+BEFORE INSERT ON Registrazione
+FOR EACH ROW
+EXECUTE PROCEDURE CKRegistrazione();
