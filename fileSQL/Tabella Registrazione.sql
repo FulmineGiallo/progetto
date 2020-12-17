1e8303190bded8f5907803362515424e2f459b56
Luca-Bianco <luca.bianco4@studenti.unina.it>
2020-12-17 22:57:47 +0100

    Update Tabella Registrazione.sql
    Aggiunto il vincolo di unicità nella tabella registrazzione

--- fileSQL/Tabella Registrazione.sql
+++ fileSQL/Tabella Registrazione.sql
@@ -6,4 +6,5 @@ idAmbito 	INTEGER,
 CONSTRAINT Fk1FOREIGN KEY (CF) REFERENCES Impiegato (CF) ON UPDATE CASCADE, ON DELETE CASCADE,
 CONSTRAINT Fk2 FOREIGN KEY (idAmbito) REFERENCES Ambiti (idAmbito) ON UPDATE CASCADE, ON DELETE CASCADE,
 CONSTRAINT CKRuolo CHECK (Ruolo IN ('Project Manager', 'Partecipante', 'Programmatore', 'Grafico', 'Analista', 'Tecnico', 'Consulente'))
+CONSTRAINT uq1 UNIQUE(cf, idprogetto)
 )	
