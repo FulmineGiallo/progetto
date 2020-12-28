cdbfa9d9b825b3c25d26ba3792d37fec2f614b25
mgcarofano <m.carofano@studenti.unina.it>
2020-12-28 23:33:29 +0100

    mi scoccio

--- JAVA-GUI/v1/Progettov1/src/controller/Controller.java
+++ JAVA-GUI/v1/Progettov1/src/controller/Controller.java
@@ -22,8 +22,16 @@ public class Controller
         frame = new Registrazione(writer);
     }
     
+
+    
     public void inviaCurriculum(javafx.event.ActionEvent actionEvent) throws Exception {
         Stage stage = (Stage)invia.getScene().getWindow();
         frame.start(stage);
     }
+    
+    public void CambiaLabel (javafx.event.ActionEvent actionEvent) {
+    	cambia.setText("ciao");
+    }
+    
+
 }
