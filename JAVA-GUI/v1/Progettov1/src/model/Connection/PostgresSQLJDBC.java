e3b3f72c97f786bd42727f0849058ddfaf28685d
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2021-01-03 17:48:27 +0100

    Creata classe per la connessione a Postegre

--- JAVA-GUI/v1/Progettov1/src/model/Connection/PostgresSQLJDBC.java
+++ JAVA-GUI/v1/Progettov1/src/model/Connection/PostgresSQLJDBC.java
@@ -6,17 +6,18 @@ import java.sql.SQLException;
 
 public class PostgresSQLJDBC
 {
-    public final String url= "database-oobd.c9kspmggklai.eu-west-2.rds.amazonaws.com";
+    public final String url= "database-oobd.c9kspmggklai.eu-west-2.rds.ama  zonaws.com";
     public final String user = "postgres";
     public final String password = "Postgresebello1";
-    
+
 
     public void connect()
     {
         try
         {
-
+            Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url,user,password);
+
 
             if(connection != null)
             {
@@ -25,7 +26,7 @@ public class PostgresSQLJDBC
             else
                 System.out.println("Connessione fallita");
         }
-        catch (SQLException err)
+        catch (SQLException | ClassNotFoundException err)
         {
             err.printStackTrace();
         }
