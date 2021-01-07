0d74b337bc5655c51388f8a44e332e6698cb7e97
Luca-Bianco <luca.bianco4@studenti.unina.it>
2021-01-07 13:47:50 +0100

    aggiunta azione per tornare dalle valutazioni allla home
    ho aggiunto questa operazione ma ancora non è completa perché bisogna passare l'impiegato

--- JAVA-GUI/v1/Progettov1/src/controller/ControllerValutazioni.java
+++ JAVA-GUI/v1/Progettov1/src/controller/ControllerValutazioni.java
@@ -1,11 +1,21 @@
 package controller;
 
+import java.io.PrintWriter;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import view.HomePageBenvenuto;
+import view.HomePageImpiegato;
+import view.HomePageValutazioni;
 
 public class ControllerValutazioni {
+	
+	HomePageImpiegato homeImpiegato;
+	
 
     @FXML
     private ListView<?> ValutazioniListView;
@@ -25,6 +35,13 @@ public class ControllerValutazioni {
     @FXML
     private Button TornaAllaHomeButton;
     
-    
+   public void backhomeimpiegato(ActionEvent event) throws Exception {
+	   
+	   	PrintWriter writer = null;
+   		homeImpiegato = new HomePageImpiegato(writer, null);
+
+   		Stage stage = (Stage)TornaAllaHomeButton.getScene().getWindow();
+   		homeImpiegato.start(stage);
+    }
 
 }
