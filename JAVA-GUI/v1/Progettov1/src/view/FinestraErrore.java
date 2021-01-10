7f618ac874ff73450025f02d6a46cbc0aa1b39d8
mgcarofano <m.carofano@studenti.unina.it>
2021-01-10 17:04:04 +0100

    Aggiornamento GUI
    +aggiornata finestra di errore, adesso supporta anche messaggi lunghi grazie alla presenza della TextArea (quindi, utilizzabile per tutti i messaggi di errore possibili)

--- JAVA-GUI/v1/Progettov1/src/view/FinestraErrore.java
+++ JAVA-GUI/v1/Progettov1/src/view/FinestraErrore.java
@@ -19,17 +19,20 @@ public class FinestraErrore {
         this.writer = writer;
     }
 	
-    public void start(Stage window) throws Exception
+    public void start(Stage window, String tipoErrore) throws Exception
     {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("view/fxml/FinestraErrore.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
+        
+        window.hide();
         window.setScene(scene);
         
         controllerFinestraErrore = loader.getController();
+        //controllerFinestraErrore.inizializza();
         
         window.setTitle("Errore!");
-        window.setResizable(true);
+        window.setResizable(false);
         window.setMinWidth(400.0);
         window.setMinHeight(200.0);
         window.centerOnScreen();
