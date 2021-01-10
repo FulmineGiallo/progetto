c208185d6b65cca73490354107ef4439aa3d0ec1
mgcarofano <m.carofano@studenti.unina.it>
2021-01-10 17:19:57 +0100

    Aggiornamento GUI
    +reintrodotta MessaggioErroreLabel
    +quando si clicca MostraDettagli si visualizza la textarea e si nasconde la label e cambia il testo del bottone in "Nascondi Dettagli" e viceversa (da fare)

--- JAVA-GUI/v1/Progettov1/src/controller/ControllerFinestraErrore.java
+++ JAVA-GUI/v1/Progettov1/src/controller/ControllerFinestraErrore.java
@@ -1,5 +1,33 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import javafx.event.ActionEvent;
+
 public class ControllerFinestraErrore {
+
+    @FXML private AnchorPane 	FinestraErrore;
+    @FXML private GridPane 		MessaggioBox;
+    @FXML private VBox 			TestoBox;
+    @FXML private Label 		AttenzioneLabel;
+    @FXML private TextArea 		MessaggioErroreTA;
+    @FXML private Label 		MessaggioErroreLabel;
+    @FXML private VBox 			ImmagineBox;
+    @FXML private ImageView 	AttenzioneIV;
+    @FXML private GridPane 		ButtonBar;
+    @FXML private Button 		DettagliButton;
+    @FXML private Button 		OkButton;
+
+    @FXML
+    void MostraNascondiDettagli(ActionEvent event) {
+
+    }
 
 }
