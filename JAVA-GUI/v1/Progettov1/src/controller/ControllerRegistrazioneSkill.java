cd9ee37744ffe12f874566c6dd28279f8c0b851a
Luca-Bianco <luca.bianco4@studenti.unina.it>
2021-01-15 18:57:21 +0100

    Update ControllerRegistrazioneSkill.java
    Se viene cambiata la voce nuovoTitoloTF scompare

--- JAVA-GUI/v1/Progettov1/src/controller/ControllerRegistrazioneSkill.java
+++ JAVA-GUI/v1/Progettov1/src/controller/ControllerRegistrazioneSkill.java
@@ -106,6 +106,9 @@ public class ControllerRegistrazioneSkill {
                     {
                 		NuovoTitoloTF.setVisible(true);	
                     }
+                	else {
+                		NuovoTitoloTF.setVisible(false);	
+                	}
                 
             });
             
