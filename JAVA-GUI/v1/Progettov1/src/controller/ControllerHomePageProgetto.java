d5b102e7e7d15cc94c6f96ac9ab0578493f741bd
Luca-Bianco <luca.bianco4@studenti.unina.it>
2021-01-27 17:15:26 +0100

    Update ControllerHomePageProgetto.java
    Adesso se si clicca su un partecipante di un progetto si possono vedere le sue informazioni

--- JAVA-GUI/v1/Progettov1/src/controller/ControllerHomePageProgetto.java
+++ JAVA-GUI/v1/Progettov1/src/controller/ControllerHomePageProgetto.java
@@ -3,20 +3,27 @@ package controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Connection.DBConnection;
 import model.Dao.ProgettoDao;
+import model.Dao.SkillDao;
+import model.Dao.TitoloDAO;
 import model.DaoInterface.ProgettoDaoInterface;
 import model.Impiegato;
 import model.Progetto;
+import model.Titolo;
 import view.HomePageImpiegato;
 
 import java.sql.Connection;
@@ -49,9 +56,40 @@ public class ControllerHomePageProgetto {
     @FXML private AnchorPane 			DescrizioneProgettoImpiegatoBox;
     @FXML private AnchorPane 			DescrizioneProgettoImpiegatoPane;
     
+    @FXML
+    private VBox ProjectManagerBox;
+
+    @FXML
+    private Label NomePartecipanteLabel;
+
+    @FXML
+    private Label SkillComboBoxLabel;
+
+    @FXML
+    private ComboBox<Titolo> SkillComboBox;
+
+    @FXML
+    private VBox SkillBox;
+
+    @FXML
+    private Label TitoloSkillLabel;
+
+    @FXML
+    private Label DataCertificazioneSkillLabel;
+
+    @FXML
+    private TextArea DescrizioneSkillTA;
+
+    @FXML
+    private Button RimuoviImpiegatoButton;
+    
+    
     private HomePageImpiegato homePageImpiegato;
     private Stage window;
     private Stage popup;
+    
+    private TitoloDAO titoloDAO;
+    private SkillDao SkillDAO;
     
     Progetto progetto;
     Impiegato impiegato;
@@ -90,7 +128,67 @@ public class ControllerHomePageProgetto {
         NomeProgettoLabel.setText(progetto.getTitolo());
         lista = progettoDao.getPartecipanti(progetto);
         ListaPartecipantiLV.setItems(lista);
+        updateInfoImpiegato();
     }
+    
+    public void updateInfoImpiegato()
+    {
+        ListaPartecipantiLV.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                IstruzioniBox.setVisible(false);
+                
+                DescrizioneProgettoImpiegatoBox.setVisible(true);
+                NomeProgettoLabel.setVisible(true);
+                
+                DataCertificazioneSkillLabel.setVisible(false);
+                DescrizioneSkillTA.setVisible(false);
+                NomePartecipanteLabel.setText(ListaPartecipantiLV.getSelectionModel().getSelectedItem().getNome() + " " + ListaPartecipantiLV.getSelectionModel().getSelectedItem().getCognome());
+
+                
+                
+            	try
+                {
+                    titoloDAO = new TitoloDAO(connection);
+                    SkillDAO = new SkillDao(connection);
+                    
+                    SkillComboBox.setItems(titoloDAO.titoliListImpiegato(ListaPartecipantiLV.getSelectionModel().getSelectedItem()));
+
+                    
+                    SkillComboBox.getSelectionModel().selectedItemProperty().addListener( (options, oldValue, newValue) -> {
+
+                    	SkillBox.setVisible(true);
+
+                    if(SkillComboBox.getSelectionModel().getSelectedItem() != null) {	
+                    	TitoloSkillLabel.setVisible(true);
+                    	TitoloSkillLabel.setText(SkillComboBox.getSelectionModel().getSelectedItem().toString());
+                    	
+                    	DataCertificazioneSkillLabel.setVisible(true);
+                    	DescrizioneSkillTA.setVisible(true);
+                    	try {
+							DescrizioneSkillTA.setText(SkillDAO.descrizioneCertificazione(SkillComboBox.getSelectionModel().getSelectedItem().toString(), ListaPartecipantiLV.getSelectionModel().getSelectedItem()));
+                    		DataCertificazioneSkillLabel.setText(SkillDAO.dataCertificazione(SkillComboBox.getSelectionModel().getSelectedItem().toString(), ListaPartecipantiLV.getSelectionModel().getSelectedItem()));
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+                    	
+                    }else {
+                    	TitoloSkillLabel.setVisible(false);
+                    	DataCertificazioneSkillLabel.setVisible(false);
+                    	DescrizioneSkillTA.setVisible(false);
+                    }
+                    
+                   });
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                
+                
+            }
+        });
+    }
+    
     
     @FXML
     private void backHomePageImpiegato(ActionEvent event) throws Exception
